50934f613327c8bd0259aeb0b06f370244c58613
Markus Lång <markus@speechgrinder.com>
2019-02-19 21:39:38 +0200

    Change package name to not contain underscores

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from distutils.core import setup
 
 setup(
-    name='logging_py',
-    version='0.6',
+    name='logger',
+    version='0.7',
     install_requires=['sanic>18'],
     py_modules=['logger'],
 )
