29a5cc6867999742b6ae2ff7ec9c18ba122600d1
Markus Lång <markus@speechly.com>
2020-09-15 10:58:52 +0300

    Update versions

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='logger',
-    version='0.91',
-    extras_requires=['sanic>=19.6'],
+    version='0.93',
+    summary='Sane logging for python in Kubernetes',
     packages=['logger'],
+    install_requires=['sanic>20.6'],
+    python_requires='>=3.8'
 )
